acf6f2d2f20a5243b0f756159b00e504edba750c
Daniel Arguello <85189501+Arguello98@users.noreply.github.com>
2023-09-14 20:28:00 -0600

    version 2.1
    se queda pegado

--- main.c
+++ main.c
@@ -11,11 +11,12 @@
 #define MSG_SIZE 128
 #define NUM_PROCESS 5
 #define BUFFER_SIZE 8192
-
+#define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
 //Estructura del mensaje
 struct message {
   long type;
   int filePosition;
+  int Termino;
   char content[MSG_SIZE];
 } msg;
 
@@ -27,26 +28,39 @@ int main(int argc, char *argv[]){
     int msqid = msgget(msqkey, IPC_CREAT | S_IRUSR | S_IWUSR);
 
     regex_t regex;
-    const char* const re = "John.*o";
+    const char* const re = "The";
+
+    regmatch_t  pmatch[1];
 
     FILE* fp;
     fp = fopen("quijote.txt", "r");
     if(fp==NULL){ exit(1); }
 
+    char*inicioLinea;
+
+    msg.Termino = 0;
     if (regcomp(&regex, re, REG_NEWLINE))
         exit(EXIT_FAILURE);
 
     for (int i=1; i<=NUM_PROCESS; i++) {
         if (fork() == 0){
-            for(int z=0; z<2; z++){
+            while(1){
                 msgrcv(msqid, &msg, MSG_SIZE, i, 0);
-
+                if(msg.Termino ==1){
+                    fclose(fp);
+                    exit(0);
+                }
                 fseek(fp, msg.filePosition, SEEK_SET);
 
                 char buffer[BUFFER_SIZE];
+                char *inicioLInea  = buffer;
+                char *finalLiena = buffer;
+                char *inicioArchivo = buffer;
 
                 fread(buffer, sizeof(char), sizeof(buffer), fp);
-
+                if(feof(fp) != 0 ){
+                    msg.Termino =1;
+                }
                 actualPosition = msg.filePosition + BUFFER_SIZE;
 
                 int j = 0;
@@ -58,29 +72,65 @@ int main(int argc, char *argv[]){
                 fseek(fp, j, SEEK_CUR);
 
                 actualPosition += j;      
-                printf("%d : %s\n", i, buffer);        
-
                 msg.filePosition = actualPosition;
                 msg.type = 100;
                 msgsnd(msqid, (void *)&msg, sizeof(msg.filePosition), IPC_NOWAIT);
+
+
+                //evaluar 
+                regoff_t    off, len;
+                for (unsigned int i = 0; ; i++) {
+                    if (regexec(&regex, inicioLInea, ARRAY_SIZE(pmatch), pmatch, 0))
+                        break;
+
+                    len = pmatch[0].rm_eo - pmatch[0].rm_so;
+                    
+                     
+                    finalLiena = inicioLInea;
+                    while( *(inicioLInea + pmatch[0].rm_so) != '\n' && inicioLInea + pmatch[0].rm_so !=  inicioArchivo){
+                        pmatch[0].rm_so--;
+                        len++;
+                    }
+                    while( *(finalLiena + pmatch[0].rm_eo) != '\n' && finalLiena + pmatch[0].rm_eo != inicioArchivo + BUFFER_SIZE -1){
+                        len++;
+                        pmatch[0].rm_eo++;
+                    }
+
+
+                    printf("substring = \"%.*s\"\n", len, inicioLInea + pmatch[0].rm_so);
+
+                    inicioLInea += pmatch[0].rm_so + len;
+                }
             }
             exit(0); 
         }
     }
-
-    for(int z=0; z<2; z++){
+    
+    while(1){
         for (int i=1; i<=NUM_PROCESS; i++){
             msg.type = i;
             msg.filePosition = actualPosition;
             msgsnd(msqid, (void *)&msg, sizeof(msg.filePosition), IPC_NOWAIT);
 
             msgrcv(msqid, &msg, MSG_SIZE, 100, 0);
-            printf("padre %d", actualPosition);
+            if(msg.Termino ==1){
+                for (int j=1; j<=NUM_PROCESS; j++)
+                {
+                    msg.Termino =1;
+                    msgsnd(msqid, (void *)&msg, sizeof(msg.filePosition), IPC_NOWAIT);
+                }
+                for (int j=1; j<=NUM_PROCESS; j++)
+                {
+                    wait(&status);
+                }
+                fclose(fp);
+                exit(0); 
+                
+            }
             actualPosition = msg.filePosition;
         }
     }
 
     //wait(&status);
-    fclose(fp);
-    exit(0); 
+
 }
