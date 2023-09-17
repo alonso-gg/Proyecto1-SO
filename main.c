957847b55ac26f68d9ffcb09919fb96631192fb8
Daniel Arguello <85189501+Arguello98@users.noreply.github.com>
2023-09-16 18:29:46 -0600

    cambios algo raro
    algo raro

--- main.c
+++ main.c
@@ -29,9 +29,10 @@ int main(int argc, char *argv[]){
     pid_t fileSeekers[NUM_PROCESS];
     int status, actualPosition = 0;
 
-    key_t msqkey = 976;
+    key_t msqkey = 771;
     msgctl(msqkey, IPC_RMID, NULL);
-    int msqid = msgget(msqkey, IPC_CREAT | S_IRUSR | S_IWUSR);
+    sleep(1);
+    int msqid = msgget(msqkey, IPC_CREAT | 0666);
 
     regex_t regex;
     const char* const re = "Capítulo";
@@ -51,21 +52,22 @@ int main(int argc, char *argv[]){
 
     for (int i=1; i<NUM_PROCESS; i++) {
         if (fork() == 0){
+            char buffer[BUFFER_SIZE];
+            char *inicioLInea  = buffer;
+            char *finalLiena = buffer;
+            char *inicioArchivo = buffer;
             while(1){
                 msgrcv(msqid, &msg, MSG_SIZE, i, 0);
                 if(msg.mensaje[1] ==1){
                     fclose(fp);
                     exit(0);
                 }
-                printf("Este hijo esta por la linea: %i \n", msg.mensaje[0]);
+                //printf("Este hijo esta por la linea: %i \n", msg.mensaje[0]);
                 fseek(fp, msg.mensaje[0], SEEK_SET);
 
-                char buffer[BUFFER_SIZE];
-                char *inicioLInea  = buffer;
-                char *finalLiena = buffer;
-                char *inicioArchivo = buffer;
-
-                fread(buffer, sizeof(char), sizeof(buffer), fp);
+                if(fread(buffer, sizeof(char), sizeof(buffer), fp) != BUFFER_SIZE){
+                    msg.mensaje[1] =1;
+                };
                 
                 actualPosition = msg.mensaje[0] + BUFFER_SIZE;
                 
@@ -77,22 +79,17 @@ int main(int argc, char *argv[]){
                 }
                 
                 fseek(fp, j, SEEK_CUR);
-
-                
-                if(feof(fp) != 0 ){
-                    msg.mensaje[1] =1;
-                }
                 
                 actualPosition += j;      
                 msg.mensaje[0] = actualPosition;
-                printf("Este hijo esta por la linea: %i \n", msg.mensaje[0]);
-                msg.mensaje[1]=0;
+               // printf("Este hijo esta por la linea: %i \n", msg.mensaje[0]);
+                msg.mensaje[2]=0;
                 msg.mensaje[3]=0;
                 msg.type = 100;
                 msgsnd(msqid, (void *)&msg, sizeof(msg.mensaje) , IPC_NOWAIT);
                 
                 //evaluar 
-                /*
+                
                 regoff_t    off, len;
                 for (unsigned int k = 0; ; k++) {
                     if (regexec(&regex, inicioLInea, ARRAY_SIZE(pmatch), pmatch, 0))
@@ -110,24 +107,23 @@ int main(int argc, char *argv[]){
                         len++;
                         pmatch[0].rm_eo++;
                     }
-                    msg.imprimir = 1;
-                    msg.Termino = 0;
-                    msg.TerminoImprimir = 0;
-                    msg.hijo =i;
+                    msg.mensaje[2] = 1;
+                    msg.mensaje[1] = 0;
+                    msg.mensaje[3] = 0;
+                    msg.mensaje[4] =i;
                     msg.type = 100;
-                    msgsnd(msqid, (void *)&msg, sizeof(msg.filePosition) + sizeof(msg.hijo) + sizeof(msg.imprimir) + sizeof(msg.Termino) + sizeof(msg.TerminoImprimir), 0);
+                    msgsnd(msqid, (void *)&msg, sizeof(msg.mensaje) , 0);
                     printf("%.*s\"\n", len, inicioLInea + pmatch[0].rm_so);
                     
                     inicioLInea += pmatch[0].rm_so + len;
                 }
                 
-                msg.imprimir = 0;
-                msg.Termino = 0;
-                msg.TerminoImprimir = 1;
-                msg.hijo =i;
+                msg.mensaje[2] = 0;
+                msg.mensaje[1] = 0;
+                msg.mensaje[3] = 1;
+                msg.mensaje[4] =i;
                 msg.type = 100;
-                msgsnd(msqid, (void *)&msg, sizeof(msg.filePosition) + sizeof(msg.hijo) + sizeof(msg.imprimir) + sizeof(msg.Termino) + sizeof(msg.TerminoImprimir), 0);
-                */
+                msgsnd(msqid, (void *)&msg, sizeof(msg.mensaje) , IPC_NOWAIT);
             }
         }else{
             hijos[i-1]=0;
@@ -141,15 +137,14 @@ int main(int argc, char *argv[]){
     int trabajando =1;
     int hijosTrabajando =1;
 
-    while(trabajando ==1 /*|| hijosTrabajando ==1*/){
+    while(trabajando ==1 || hijosTrabajando ==1){
         msgrcv(msqid, &msg, MSG_SIZE, 100, 0);
         if(msg.mensaje[1] ==1){
             trabajando=0;
             printf("Hijo termino \n");
-            fflush(stdout);
         }
-        /*else if(msg.TerminoImprimir == 1){
-            hijos[msg.hijo -1] = 0;
+        else if(msg.mensaje[3] == 1){
+            hijos[msg.mensaje[4] -1] = 0;
             hijosTrabajando =0;
             for (int j = 0; j < NUM_PROCESS; j++)
             {
@@ -157,15 +152,14 @@ int main(int argc, char *argv[]){
                     hijosTrabajando=1;
                 }
             }
-        } if(msg.imprimir ==1){
-            fflush(stdout); 
-        }*/
+        } if(msg.mensaje[2] ==1){
+            
+        }
         else{
             hijos[i-1]=1;
             i++;
             if(i == NUM_PROCESS){
                 i=1;
-                trabajando = 0;
             }
             msg.type = i;
             actualPosition = msg.mensaje[0];
@@ -182,7 +176,7 @@ int main(int argc, char *argv[]){
         msg.mensaje[1] =1;
         msgsnd(msqid, (void *)&msg, sizeof(msg.mensaje) , 0);
     }
-     for (int j=1; j<NUM_PROCESS; j++)
+    for (int j=1; j<NUM_PROCESS; j++)
     {
         wait(&status);    
     }
